11207f8f925f5cc141b549bd4e5a48412e01369c
Kkkairat <rm1697hala@gmail.com>
2017-10-04 14:46:20 +0600

    mym.java файлында isOdd() методына өзгеріс енгіздім. int емес boolean қайтарады.

--- src/s/mym.java
+++ src/s/mym.java
@@ -33,8 +33,8 @@ public class mym {
         return min (min,c);
 
     }
-    public static int isOdd(int a) {
-        return(a%2!=0);
+    public static boolean isOdd(int a) {
+        return(a%2 != 0);
     }
 
 }
