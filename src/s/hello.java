6b1b1bf521410e937ea40771d421ac01de6921d9
Kkkairat <rm1697hala@gmail.com>
2017-10-04 14:10:22 +0600

    Екінші коммит

--- src/s/hello.java
+++ src/s/hello.java
@@ -3,6 +3,7 @@ package s;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello");
-
+        System.out.println("Жұмыстың мақсаты");
+        System.out.println("GitHub жұмыс істеу");
     }
 }
